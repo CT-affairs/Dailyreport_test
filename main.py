73a34541073c08a3a0e2c4bd45eb58f71f1f4293
CT-affairs <affairs@gas.clean-techno.com>
2025-12-09 13:30:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -65,3 +65,7 @@ def callback():
 def ping():
     print("PING CALLED")
     return "pong"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
