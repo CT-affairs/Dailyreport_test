6e5a715a03ff80c9073a8536a0a1b00b134a0ac4
MotonoriMizuno <affairs@gas.clean-techno.com>
2025-12-12 14:08:10 +0900

    refactor

--- services/line_service.py
+++ services/line_service.py
@@ -1,7 +1,9 @@
 import os
 import requests
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
+JST = timezone(timedelta(hours=9), 'JST')
+ 
 class LineService:
     REPLY_ENDPOINT = "https://api.line.me/v2/bot/message/reply"
 
@@ -24,7 +26,7 @@ class LineService:
     def parse_postback(event):
         data = event["postback"]["data"]
         return {
-            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "timestamp": datetime.now(JST).strftime("%Y-%m-%d %H:%M:%S"),
             "user_id": event["source"].get("userId"),
             "answer": data.split("=")[1],
         }
